0c12631bf3ac9f94b2a9c95502c227e8f7cf89b8
kbys_tkhr <kobayashi.takahiro@solekia.co.jp>
2022-05-30 11:08:31 +0900

    ○○の対応

---
+++ TestGit/test.java
@@ -0,0 +1,11 @@
+package TestGit;
+
+public class test {
+    public static void main(String args[]){
+        System.out.println("Hello, World");
+
+        int a = 0;
+        a += 100;
+        System.out.println("a=" + a);
+    }
+}
